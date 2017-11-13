147456a28b2840d80e64be882d0d5f16e8e762a5
unknown <hartman.william@gmail.com>
2017-11-13 11:26:01 -0700

    finished contact and mobile view

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,18 @@ class Contact extends Component {
   render() {
     return (
       <div className="Contact">
-        
+        <h1 className='contact-title'>Contact</h1>
+        <div className="contact-line"></div>
+        <div className="contact-container">
+          <form action="https://formspree.io/hartman.william@gmail.com" method="POST" className='contact-form' >
+            <input placeholder='Name' type="text" name="name" className='contact-name' />
+            <input placeholder='Email' type="email" name="_replyto" className='contact-email' />
+            <input placeholder='Subject' type="subject" name="_subject" className='contact-subject' />
+            <textarea placeholder='Message' type="message" name="message" className='contact-message' />
+            <input type="submit" value="Send Message" className='contact-send' />
+            <input type="hidden" name="_next" value="williamhartman.com" />
+          </form>
+        </div>
       </div>
     );
   }
