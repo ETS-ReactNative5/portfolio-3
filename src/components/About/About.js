589913727d24b91f7240996e2a4f3e70dc52f7a6
WilliamHartman <30533594+WilliamHartman@users.noreply.github.com>
2021-12-09 15:54:59 -0800

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,9 +13,9 @@ class About extends Component {
             <div className='about-line'></div>
             <p>
               After half a decade of computer maintenence and repair work, I decided to jump back into my high school passion of programming. In September of 2017 I attended DevMountain where I learned full stack web development.
-              <br>
+              <br />
               My first full time web development position was with Microsoft where I worked for the Insiders team. My position was a mix of support and feedback gathering from the users, and web development of building a tool to help aggregate that data.
-              <br>
+              <br />
               After finishing my contract with Microsoft I was hired by Lifecenter NorthWest to be their first DevOps Engineer. I work closely with the staff to build full stack web apps where I am in control of the entire process, from conception to deployment.
             </p>
           </div>
